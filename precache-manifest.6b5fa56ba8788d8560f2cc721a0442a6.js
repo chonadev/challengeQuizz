5a60f17e68b6f420c2720cd0db99c3f6f464074c
Jhona <jhonavenegas@gmail.com>
2022-05-31 21:41:48 -0300

    Updates

---
+++ precache-manifest.6b5fa56ba8788d8560f2cc721a0442a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff7a713eea757ae80ba",
+    "url": "/challengeQuizz/static/js/main.8ff7a713.chunk.js"
+  },
+  {
+    "revision": "bc0ce6d938213eea0996",
+    "url": "/challengeQuizz/static/js/1.bc0ce6d9.chunk.js"
+  },
+  {
+    "revision": "50a3ee73134f9ac04e71",
+    "url": "/challengeQuizz/static/css/2.4907bc8a.chunk.css"
+  },
+  {
+    "revision": "50a3ee73134f9ac04e71",
+    "url": "/challengeQuizz/static/js/2.50a3ee73.chunk.js"
+  },
+  {
+    "revision": "4db7f4c31ed09347cea3",
+    "url": "/challengeQuizz/static/js/runtime~main.4db7f4c3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/challengeQuizz/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "507a68d90f38f2e1c35a3f04e48688fb",
+    "url": "/challengeQuizz/index.html"
+  }
+];
